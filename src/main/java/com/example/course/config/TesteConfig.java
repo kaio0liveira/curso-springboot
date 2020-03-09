89b8c5fd9b6e67adf0d5cb319dcd458f2cbd2a02
kaio0liveira <kaiooliveira466@gmail.com>
2020-03-08 23:18:52 -0300

    Service layer,component registration

--- src/main/java/com/example/course/config/TesteConfig.java
+++ src/main/java/com/example/course/config/TesteConfig.java
@@ -21,10 +21,9 @@ public class TesteConfig implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 	
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
+		User user1 = new User(null, "Kaio", "kaio@gmail.com", "87878787", "12345");
 		
-		userRepository.saveAll(Arrays.asList(u1,u2));
+		userRepository.saveAll(Arrays.asList(user1));
 	}
 	
 	
