7526230629ec891faf1746b83a246a14982ecc88
kaio0liveira <kaiooliveira466@gmail.com>
2020-03-10 13:21:30 -0300

    Category

--- src/main/java/com/example/course/config/TesteConfig.java
+++ src/main/java/com/example/course/config/TesteConfig.java
@@ -1,17 +1,18 @@
 package com.example.course.config;
 
 import java.time.Instant;
-
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import com.example.course.entities.enums.OrderStatus;
+
+import com.example.course.entities.Category;
 import com.example.course.entities.Order;
 import com.example.course.entities.User;
-
+import com.example.course.entities.enums.OrderStatus;
+import com.example.course.repositories.CategoryRepository;
 import com.example.course.repositories.OrderRepository;
 import com.example.course.repositories.UserRepository;
 
@@ -22,17 +23,28 @@ public class TesteConfig implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
 	@Autowired
+	
 	private OrderRepository orderRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	
 		User user1 = new User(null, "Kaio", "kaio@gmail.com", "87878787", "12345");
 		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.CANCELED, user1);
-
 		
+			
 		userRepository.saveAll(Arrays.asList(user1));
 		orderRepository.saveAll(Arrays.asList(o1));
 	}
